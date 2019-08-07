5d501cdc09153549558972356d2bcf9daff841fb
David Garcia <daganu@gmail.com>
2019-08-07 10:01:32 +0200

    eslint bump

--- extension.js
+++ extension.js
@@ -1,58 +1,64 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
+var vscode = require("vscode");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.devDocs', function () {
-        // The code you place here will be executed every time your command is executed
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return; // No open text editor
-        }
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  var disposable = vscode.commands.registerCommand(
+    "extension.devDocs",
+    function() {
+      // The code you place here will be executed every time your command is executed
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return; // No open text editor
+      }
 
-        const url = 'https://devdocs.io/';
-        let language = editor.document.languageId
-        if (language == "python") {
-            language = "python3.6";
-        }
-        
-        let text = "";
+      const url = "https://devdocs.io/";
+      let language = editor.document.languageId;
+      if (language == "python") {
+        language = "python3.6";
+      }
 
-        // First, selection for afine search, them, word under cursor.
-        var selection = editor.selection;
-        if (!selection.isEmpty) {
-            text = editor.document.getText(selection);
-        } else {
-            const position = editor.selection.active;
-            const range = editor.document.getWordRangeAtPosition(position);
-            text = editor.document.getText(range);
-        }
+      let text = "";
 
-        // Safety checks
-        if (text.length == 0) {
-            vscode.window.showErrorMessage('Null string in text variable.');
-            return;
-        }
-        if (text.indexOf('\n') >= 0) {
-          vscode.window.showErrorMessage('Multiline selection not allowed for your security.');
-          return;
-        }
+      // First, selection for afine search, them, word under cursor.
+      var selection = editor.selection;
+      if (!selection.isEmpty) {
+        text = editor.document.getText(selection);
+      } else {
+        const position = editor.selection.active;
+        const range = editor.document.getWordRangeAtPosition(position);
+        text = editor.document.getText(range);
+      }
 
-        // Open URI
-        let uri = vscode.Uri.parse(url + "#q=" + language + "%20" + text);
-        vscode.commands.executeCommand('vscode.open', uri);
-   });
+      // Safety checks
+      if (text.length == 0) {
+        vscode.window.showErrorMessage(
+          "You must select text to search it in DevDocs."
+        );
+        return;
+      }
+      if (text.indexOf("\n") >= 0) {
+        vscode.window.showErrorMessage(
+          "Multiline selection not allowed for your security."
+        );
+        return;
+      }
 
-    context.subscriptions.push(disposable);
+      // Open URI
+      let uri = vscode.Uri.parse(url + "#q=" + language + "%20" + text);
+      vscode.commands.executeCommand("vscode.open", uri);
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
