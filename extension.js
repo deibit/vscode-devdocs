3027e0e7b6980e9064351e2583a80f1aacb2c7b9
Shadab Zafar <dufferzafar0@gmail.com>
2020-02-18 18:01:40 +0530

    Don't use language while querying
    Fixes #4

--- extension.js
+++ extension.js
@@ -1,64 +1,53 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require("vscode");
+var vscode = require('vscode');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  var disposable = vscode.commands.registerCommand(
-    "extension.devDocs",
-    function() {
-      // The code you place here will be executed every time your command is executed
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        return; // No open text editor
-      }
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    var disposable = vscode.commands.registerCommand('extension.devDocs', function () {
+        // The code you place here will be executed every time your command is executed
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
 
-      const url = "https://devdocs.io/";
-      let language = editor.document.languageId;
-      if (language == "python") {
-        language = "python3.6";
-      }
+        const url = 'https://devdocs.io/';
+        let text = "";
 
-      let text = "";
+        // First, selection for afine search, them, word under cursor.
+        var selection = editor.selection;
+        if (!selection.isEmpty) {
+            text = editor.document.getText(selection);
+        } else {
+            const position = editor.selection.active;
+            const range = editor.document.getWordRangeAtPosition(position);
+            text = editor.document.getText(range);
+        }
 
-      // First, selection for afine search, them, word under cursor.
-      var selection = editor.selection;
-      if (!selection.isEmpty) {
-        text = editor.document.getText(selection);
-      } else {
-        const position = editor.selection.active;
-        const range = editor.document.getWordRangeAtPosition(position);
-        text = editor.document.getText(range);
-      }
+        // Safety checks
+        if (text.length == 0) {
+            vscode.window.showErrorMessage('Null string in text variable.');
+            return;
+        }
+        if (text.indexOf('\n') >= 0) {
+          vscode.window.showErrorMessage('Multiline selection not allowed for your security.');
+          return;
+        }
 
-      // Safety checks
-      if (text.length == 0) {
-        vscode.window.showErrorMessage(
-          "You must select text to search it in DevDocs."
-        );
-        return;
-      }
-      if (text.indexOf("\n") >= 0) {
-        vscode.window.showErrorMessage(
-          "Multiline selection not allowed for your security."
-        );
-        return;
-      }
+        // Open URI
+        let uri = vscode.Uri.parse(url + "#q=" + text);
+        vscode.commands.executeCommand('vscode.open', uri);
+   });
 
-      // Open URI
-      let uri = vscode.Uri.parse(url + "#q=" + language + "%20" + text);
-      vscode.commands.executeCommand("vscode.open", uri);
-    }
-  );
-
-  context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+}
 exports.deactivate = deactivate;
